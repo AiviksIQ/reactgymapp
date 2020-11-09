eba12a716ba2d0c7a820f539500d8ca46e09304a
Aivis <aivis.palacis@gmail.com>
2020-11-10 12:11:11 +1300

    Updates

---
+++ precache-manifest.2b5e182359c60845679b8270065c8b6c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c00b9cf9ec5007dfe31c987eed65ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9359f91c2641e265a09",
+    "url": "/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "fb5d380f195f6c6f89a1",
+    "url": "/static/css/main.8d41ab4d.chunk.css"
+  },
+  {
+    "revision": "e9359f91c2641e265a09",
+    "url": "/static/js/2.42e34c91.chunk.js"
+  },
+  {
+    "revision": "5d0756bda618e3255d388fe4749f5ec5",
+    "url": "/static/js/2.42e34c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5d380f195f6c6f89a1",
+    "url": "/static/js/main.91dda499.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "80093299d15e553fac417eaa06d0d778",
+    "url": "/static/media/adown.80093299.svg"
+  },
+  {
+    "revision": "46244b6f6197cf20486c0392aaa738f1",
+    "url": "/static/media/contacts.46244b6f.jpg"
+  },
+  {
+    "revision": "172e86d14f33d4f473a08af8f66b8751",
+    "url": "/static/media/dumbells.172e86d1.jpg"
+  },
+  {
+    "revision": "a6ed7a2698678baa6d054aa378ddf953",
+    "url": "/static/media/dumbells900.a6ed7a26.jpg"
+  },
+  {
+    "revision": "795ea9774c61434ac710408ba8626894",
+    "url": "/static/media/email.795ea977.svg"
+  },
+  {
+    "revision": "c5143ef17dd094a46daa3422d30ca53a",
+    "url": "/static/media/gym1.c5143ef1.jpg"
+  },
+  {
+    "revision": "e722effba5c2f1e1335f34ebb71e2945",
+    "url": "/static/media/hero.e722effb.jpg"
+  },
+  {
+    "revision": "77cc5b84986cda957c4ee80fc16f97ba",
+    "url": "/static/media/heroMid.77cc5b84.jpg"
+  },
+  {
+    "revision": "d43a2b016233ef37fdc57504dd4e6f36",
+    "url": "/static/media/home.d43a2b01.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fadc384b3e9b2ae785b25c7db92b56c",
+    "url": "/static/media/phone-call.4fadc384.svg"
+  },
+  {
+    "revision": "5c34a329981e3a504b2be520f9f2c375",
+    "url": "/static/media/video.5c34a329.mp4"
+  }
+]);
