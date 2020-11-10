da4b1fa782c53790f9f016662d2b889c715ff8f4
Aivis <aivis.palacis@gmail.com>
2020-11-10 15:59:47 +1300

    Updates

---
+++ precache-manifest.9be744ae43443195c4319d68cd2224c0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e2d389239fe61901413a5276e671fa",
+    "url": "/reactgymapp/index.html"
+  },
+  {
+    "revision": "3a0283bb37e60d678a50",
+    "url": "/reactgymapp/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "ad917b00ee79ff96193c",
+    "url": "/reactgymapp/static/css/main.341d0ee2.chunk.css"
+  },
+  {
+    "revision": "3a0283bb37e60d678a50",
+    "url": "/reactgymapp/static/js/2.9fa825f5.chunk.js"
+  },
+  {
+    "revision": "5d0756bda618e3255d388fe4749f5ec5",
+    "url": "/reactgymapp/static/js/2.9fa825f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad917b00ee79ff96193c",
+    "url": "/reactgymapp/static/js/main.634de9e4.chunk.js"
+  },
+  {
+    "revision": "1d986c257b5a8762583e",
+    "url": "/reactgymapp/static/js/runtime-main.419d0592.js"
+  },
+  {
+    "revision": "80093299d15e553fac417eaa06d0d778",
+    "url": "/reactgymapp/static/media/adown.80093299.svg"
+  },
+  {
+    "revision": "46244b6f6197cf20486c0392aaa738f1",
+    "url": "/reactgymapp/static/media/contacts.46244b6f.jpg"
+  },
+  {
+    "revision": "172e86d14f33d4f473a08af8f66b8751",
+    "url": "/reactgymapp/static/media/dumbells.172e86d1.jpg"
+  },
+  {
+    "revision": "a6ed7a2698678baa6d054aa378ddf953",
+    "url": "/reactgymapp/static/media/dumbells900.a6ed7a26.jpg"
+  },
+  {
+    "revision": "c1435369a981e7360a84d83989ff8016",
+    "url": "/reactgymapp/static/media/email.c1435369.svg"
+  },
+  {
+    "revision": "c5143ef17dd094a46daa3422d30ca53a",
+    "url": "/reactgymapp/static/media/gym1.c5143ef1.jpg"
+  },
+  {
+    "revision": "e722effba5c2f1e1335f34ebb71e2945",
+    "url": "/reactgymapp/static/media/hero.e722effb.jpg"
+  },
+  {
+    "revision": "77cc5b84986cda957c4ee80fc16f97ba",
+    "url": "/reactgymapp/static/media/heroMid.77cc5b84.jpg"
+  },
+  {
+    "revision": "d43a2b016233ef37fdc57504dd4e6f36",
+    "url": "/reactgymapp/static/media/home.d43a2b01.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgymapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c32962d133c26e3228daaac50579a582",
+    "url": "/reactgymapp/static/media/phone-call.c32962d1.svg"
+  },
+  {
+    "revision": "5c34a329981e3a504b2be520f9f2c375",
+    "url": "/reactgymapp/static/media/video.5c34a329.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd5be2512011cb94078020d18341b0e2.js"
+  "/reactgymapp/precache-manifest.9be744ae43443195c4319d68cd2224c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactgymapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
