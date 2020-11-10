718b1fe23cb2837b2ebfb4f3456df1bc90019113
Aivis <aivis.palacis@gmail.com>
2020-11-10 18:42:01 +1300

    m

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,15 @@
-// import './Loader.css';
+import './Loader.css';
 
-// const loader = document.querySelector('.loader');
-// const main = document.querySelector('.main');
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
 
-// function init() {
-//     setTimeout(() => {
-//         loader.style.opacity = 0;
-//         loader.style.display = 'none';
-//         main.style.display = 'block';
-//         setTimeout(() => (main.style.opacity = 1), 50);
-//     }, 2000);
-// }
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+        main.style.display = 'block';
+        setTimeout(() => (main.style.opacity = 1), 50);
+    }, 4000);
+}
 
-// init();
+init();
